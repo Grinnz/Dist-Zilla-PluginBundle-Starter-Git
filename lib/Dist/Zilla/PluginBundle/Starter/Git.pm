d4f36ab381243ec5b73869183c4b1c7a4b090544
Dan Book <grinnz@grinnz.com>
2020-12-29 20:14:37 -0500

    Bump version

--- lib/Dist/Zilla/PluginBundle/Starter/Git.pm
+++ lib/Dist/Zilla/PluginBundle/Starter/Git.pm
@@ -4,7 +4,7 @@ use Moose;
 extends 'Dist::Zilla::PluginBundle::Starter';
 use namespace::clean;
 
-our $VERSION = 'v5.0.0';
+our $VERSION = 'v5.0.1';
 
 has '+revision' => (
   default => sub { $_[0]->payload->{revision} // 3 },
